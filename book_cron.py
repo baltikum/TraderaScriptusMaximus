3cce75b80322030c11813f746167a461f3c54612
baltikum <mwdavidsson@outlook.com>
2023-02-01 19:51:24 +0100

    Raderat

--- book_cron.py
+++
@@ -1,6 +0,0 @@
-    booking  = cron.new(command='/usr/bin/echo snopp',
-                    comment=str(item_to_bid_on.objectNumber))
-    booking.month.on(item_to_bid_on.end_month)
-    booking.day.on(item_to_bid_on.end_day)
-    booking.hour.on(item_to_bid_on.end_hour)
-    booking.minute.on(item_to_bid_on.end_minute)
